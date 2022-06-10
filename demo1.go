afe4caaef9f78f3e98f358bf8927f4c2644a630e
chen2349 <chenxu_2349@qq.com>
2022-06-10 16:00:29 +0800

    22.6.10

--- demo1.go
+++ demo1.go
@@ -1,12 +1,12 @@
-package GoLearning
+package main
 
-import (
-	"fmt"
-	_ "fmt"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("hello world1")
 	println("hello world2")
-
+	var a string = "NULL"
+	var b int = 20
+	var c, d = 0.5, "OK"
+	println(a, b, c, d)
 }
