b02ca06479d9bfba23c551b3a34890d7c3a4d4bf
chenxu <chenxu_2349@qq.com>
2023-03-27 18:26:40 +0800

    简易服务器

--- demo1.go
+++ demo1.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("hello world11111")
@@ -9,4 +13,19 @@ func main() {
 	var b int = 20
 	var c, d = 0.5, "OK123"
 	println(a, b, c, d)
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	//fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
 }
