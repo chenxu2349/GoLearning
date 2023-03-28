5aef956de6295d19ca36006d92cf81af4ae55d07
chenxu <chenxu_2349@qq.com>
2023-03-28 13:27:05 +0800

    简易服务器

--- demo1.go
+++ demo1.go
@@ -7,13 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("hello world11111")
-	println("hello world2")
-	var a string = "NULL"
-	var b int = 20
-	var c, d = 0.5, "OK123"
-	println(a, b, c, d)
-
+	basicLearning()
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/hello", helloHandler)
 	log.Fatal(http.ListenAndServe(":9999", nil))
@@ -28,4 +22,17 @@ func helloHandler(w http.ResponseWriter, req *http.Request) {
 	for k, v := range req.Header {
 		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 	}
+}
+
+func basicLearning() {
+	fmt.Println("hello world11111")
+	println("hello world2")
+	var a string = "NULL"
+	var b int = 20
+	var c = "OK123"
+	var d, e, f, g, h = 0.5, 1.e+0, 6.6748e-11, .1314e+5, 1e6 // 浮点型
+	var ch rune = 'a'
+	aa, bb := 1, "中国" // 直接赋值
+	println(a, b, c, d, e, f, g, h, ch)
+	println(aa, bb)
 }
