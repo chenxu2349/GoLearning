0f706d03f2adb3bb794211824d8f1e9abaf07ad6
chenxu <chenxu_2349@qq.com>
2023-04-16 21:37:33 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # GoLearning
 Start Learning Golang
+
+## Web框架
+
+Gin   
+https://github.com/gin-gonic/gin
+
+Beego   
+https://github.com/beego/beego
+
+Iris  
+https://github.com/kataras/iris
+
+Echo  
+https://github.com/labstack/echo
+
+GoFrame  
+https://github.com/gogf/gf
+
+## 练手项目
+
+id-maker  
+项目地址： https://github.com/yongxinz/id-maker  
+
+Go 开发的一款分布式唯一 ID 生成系统。
+代码量少，项目整体难度小，通过这个项目可以学习到 httpServer，gRPC 相关知识。
+
+<br>
+
+cache2go  
+项目地址： https://github.com/muesli/cache2go
+
+比较简单的一个缓存库，代码量很少，适合新手学习。通过这个项目可以学习到锁、goroutine 等知识。
+
+<br>
+
+rosedb  
+项目地址： https://github.com/flower-corp/rosedb
+
+rosedb 是一个稳定、高性能、快速、内嵌的 k-v 存储引擎，支持多种数据结构，包含 String、List、Hash、Set、Sorted Set，接口名称风格和 Redis 类似。
+项目使用纯 Golang 实现，Go 语言的绝大部分基础知识点都包含了，相信一定是学习和巩固 Golang 的很不错的项目。除此之外，代码中还有一些数据结构的实现，所以同时也能学习一下数据结构的知识。
+
+<br>
+
+wechat-go  
+项目地址： https://github.com/songtianyi/wechat-go
+
+这是微信 Web 版 API 的 Go 语言实现，模拟微信网页版的登录／联系人／消息收发等功能，可以完全接管微信收到的消息，并定制自己的发送内容。  
+我觉得这个项目还是挺有趣的，用来操作一下微信，能够体会到更多学习的乐趣。
+
+<br>
+
+gin-vue-admin  
+项目地址： https://github.com/flipped-aurora/gin-vue-admin
+
+gin-vue-admin 是一个使用 gin + vue 进行极速开发的全栈后台管理系统，后台使用 gin 框架，前端主要使用 vue。
+项目的目录结构非常清晰，包名语义化，能够帮助你快速上手学习。
+
+这个项目还使用到了一些流行的 Go 开源库及中间件，例如：  
+gin：最流行的 Go 语言 Web 框架  
+gorm：数据库操作中间件  
+zap：uber 开源的 Go 语言日志库  
+redis：k-v 缓存  
+
+除此之外，这个项目还有详细的中文文档，并且配套教学视频，值得学习。
--- go_mysql.go
+++ go_mysql.go
@@ -9,6 +9,12 @@ import (
 //定义一个全局对象db
 var db *sql.DB
 
+type user struct {
+	id       int
+	username string
+	password string
+}
+
 //定义一个初始化数据库的函数
 func initDB() (err error) {
 	dsn := "root:123456@tcp(127.0.0.1:3306)/golang_db?charset=utf8mb4&parseTime=True" //不会校验账号密码是否正确
@@ -25,6 +31,7 @@ func initDB() (err error) {
 	return nil
 }
 
+//插入数据
 func insertData(name string, pwd string) {
 	sqlStr := "insert into user_tb1(username, password) values (?, ?)"
 	ret, err := db.Exec(sqlStr, name, pwd)
@@ -38,6 +45,51 @@ func insertData(name string, pwd string) {
 		return
 	}
 	fmt.Printf("get last insert id success, the id is : %v\n", theID)
+}
+
+//查询单行数据
+func queryOneRow() {
+	sqlStr := "select id, username, password from user_tb1 where id = ?"
+	var u user
+	//查询后调用scan方法，否则持有的数据库链接不会被释放
+	err := db.QueryRow(sqlStr, 1).Scan(&u.id, &u.username, &u.password)
+	if err != nil {
+		fmt.Printf("scan failed, err : %v\n", err)
+		return
+	}
+	fmt.Printf("id : %d, name : %s, password : %s\n", u.id, u.username, u.password)
+}
+
+//查询多行数据
+func queryMultiRow() {
+	sqlStr := "select id, username, password from user_tb1 where id > ?"
+	rows, err := db.Query(sqlStr, 0)
+	if err != nil {
+		fmt.Printf("query failed! err : %v\n", err)
+		return
+	}
+	//这步很重要
+	defer rows.Close()
+
+	for rows.Next() {
+		var u user
+		err := rows.Scan(&u.id, &u.username, &u.password)
+		if err != nil {
+			fmt.Printf("scan failed! err : %v\n", err)
+			return
+		}
+		fmt.Printf("id : %d, name : %s, password : %s\n", u.id, u.username, u.password)
+	}
+}
+
+//更新
+func updateData() {
+
+}
+
+//删除
+func delData() {
+
 }
 
 func main() {
@@ -68,5 +120,9 @@ func main() {
 		fmt.Println("初始化成功！")
 	}
 
-	insertData("wc", "wc222!")
+	insertData("lisa", "lkss2212")
+
+	queryOneRow()
+
+	queryMultiRow()
 }
